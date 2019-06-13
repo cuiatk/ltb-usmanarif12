58b48781d1a1f0221b83fec87d480ed0ff8a87d2
usman Arif <usmanarif83@gmail.com>
2019-06-13 10:36:03 +0500

    TODO1

--- src/labTerminalB/Driver.java
+++ src/labTerminalB/Driver.java
@@ -11,7 +11,7 @@ public class Driver {
 	 */
 	public static void main(String[] args) {
 		String input = "The <b>Good</b>, the <i>Bad</i>, and the <strong>Ugly</strong>";
-		String regex = "TODO 1";
+		String regex = "<[^>]*>";
 		String output = input.replaceAll(regex, "");
 		System.out.println(output);
 		
